57c73d4afe69e5316eb970971d2104275c19f636
Matias-07 <matias.ferrari.m@gmail.com>
2024-02-27 20:50:18 -0300

    First step in the proyect

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express'); 
+
+const app = express(); 
+
+const path = require('path')
+const publicPath = path.resolve(__dirname, './public')
+
+app.use(express.static(publicPath))
+
+app.listen(3002, ()=>{
+    console.log('la pagina esta funcionando');
+})
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, './views/index.html'))
+})
